3b5cd1e754c461a35215018403af50ff1cb9cc7c
Tristan Schneider <maneugeant@gmail.com>
2017-09-17 18:23:45 -0700

    Sync on worker pool

--- syx/gfx/threading/WorkerPool.cpp
+++ syx/gfx/threading/WorkerPool.cpp
@@ -2,18 +2,26 @@
 #include "threading/WorkerPool.h"
 #include "threading/Task.h"
 
-WorkerPool::WorkerPool(size_t threadCount)
+WorkerPool::WorkerPool(size_t workerCount)
   : mTerminate(false)
-  , mWorkerCount(threadCount) {
+  , mWorkers(nullptr)
+  , mWorkerCount(workerCount) {
 
-  mWorkers = new std::thread[threadCount];
-  for(size_t i = 0; i < threadCount; ++i)
-    mWorkers[i] = std::thread(&WorkerPool::_workerLoop, this);
+  if(mWorkerCount) {
+    mWorkers = new std::thread[workerCount];
+    for(size_t i = 0; i < workerCount; ++i)
+      mWorkers[i] = std::thread(&WorkerPool::_workerLoop, this);
+  }
 }
 
 WorkerPool::~WorkerPool() {
   mTerminate = true;
-  _wakeAllWorkers();
+
+  //Need to have mutex when notifying so thread doesn't go to sleep right after notification
+  mTaskMutex.lock();
+  mWorkerCV.notify_all();
+  mTaskMutex.unlock();
+
   for(size_t i = 0; i < mWorkerCount; ++i)
     mWorkers[i].join();
 
@@ -25,48 +33,64 @@ void WorkerPool::queueTask(std::unique_ptr<Task> task) {
   bool canStartNow = task->canRun();
   mTaskMutex.lock();
   mTasks.push_back(std::move(task));
-  mTaskMutex.unlock();
   if(canStartNow)
-    _wakeOneWorker();
+    mWorkerCV.notify_one();
+  mTaskMutex.unlock();
+}
+
+void WorkerPool::sync(std::weak_ptr<TaskGroup> group) {
+  bool wakeWorkers = false;
+  //Normally there shouldn't be termination during sync because the syncing thread is probably the one who would tear down
+  while(!group.expired() && !mTerminate) {
+    std::unique_lock<std::mutex> taskLock(mTaskMutex);
+    //The work we're waiting on might have finished while we were grabbing mutex. If so, exit
+    if(group.expired()) {
+      //Don't forgot to wake other workers in case this thread finished a work group and that's what it was syncing on
+      if(wakeWorkers)
+        mWorkerCV.notify_all();
+      break;
+    }
+
+    _doNextTask(taskLock, wakeWorkers);
+  }
 }
 
 void WorkerPool::_workerLoop() {
   bool wakeWorkers = false;
-  std::unique_ptr<Task> task = nullptr;
   while(!mTerminate) {
-    if(!task)
-      task = _getTask();
+    //Use same lock for condition variable as tasks, so if work is being queued,
+    //a thread doesn't miss the notification then immediately wait on the condition variable
+    std::unique_lock<std::mutex> taskLock(mTaskMutex);
+    _doNextTask(taskLock, wakeWorkers);
+  }
+}
 
-    if(task) {
-      //Wake others after grabbing the task, as we don't want completion of the last task to cause other workers to steal from this
-      if(wakeWorkers)
-        _wakeAllWorkers();
+void WorkerPool::_doNextTask(std::unique_lock<std::mutex>& taskLock, bool& wakeWorkers) {
+  std::unique_ptr<Task> task = _getTask();
+  //Wake others after grabbing the task, as we don't want completion of the last task to cause other workers to steal from this
+  if(wakeWorkers)
+    mWorkerCV.notify_all();
 
-      task->run();
+  if(task) {
+    taskLock.unlock();
+    task->run();
 
-      //If completing this task unblocks other tasks, wake up other workers.
-      //Ideally you only wake others if more than one task is available, but shared_ptr can't know
-      wakeWorkers = task->isLastInGroup();
-      task = nullptr;
-    }
-    else {
+    //If completing this task unblocks other tasks, wake up other workers.
+    //Ideally you only wake others if more than one task is available, but shared_ptr can't know
+    wakeWorkers = task->isLastInGroup();
+    task = nullptr;
+  }
+  else {
+    //Termination might have been signaled as we were grabbing mutex, check before sleeping
+    if(!mTerminate) {
       //Now work now, wait until some is available
-      std::unique_lock<std::mutex> waitLock(mWorkerCVMutex);
-
-      //Work might have become available while we were grabbing the lock, if so, don't sleep now, do work
-      if(task = _getTask())
-        continue;
-      if(mTerminate)
-        break;
-
-      mWorkerCV.wait(waitLock);
+      mWorkerCV.wait(taskLock);
     }
   }
 }
 
 std::unique_ptr<Task> WorkerPool::_getTask() {
   std::unique_ptr<Task> result = nullptr;
-  std::lock_guard<std::mutex> lock(mTaskMutex);
   for(size_t i = 0; i < mTasks.size(); ++i) {
     std::unique_ptr<Task>& task = mTasks[i];
     if(task->canRun()) {
@@ -77,17 +101,4 @@ std::unique_ptr<Task> WorkerPool::_getTask() {
     }
   }
   return nullptr;
-}
-
-void WorkerPool::_wakeOneWorker() {
-  //To make sure we don't try to wake threads right before they go to sleep, lock during notification
-  mWorkerCVMutex.lock();
-  mWorkerCV.notify_one();
-  mWorkerCVMutex.unlock();
-}
-
-void WorkerPool::_wakeAllWorkers() {
-  mWorkerCVMutex.lock();
-  mWorkerCV.notify_all();
-  mWorkerCVMutex.unlock();
 }
--- syx/gfx/threading/WorkerPool.h
+++ syx/gfx/threading/WorkerPool.h
@@ -1,6 +1,7 @@
 #include "threading/IWorkerPool.h"
 
 class Task;
+class TaskGroup;
 
 class WorkerPool {
 public:
@@ -8,18 +9,18 @@ public:
   ~WorkerPool();
 
   void queueTask(std::unique_ptr<Task> task);
+  //Do work on this thread along with others until this group is done
+  void sync(std::weak_ptr<TaskGroup> group);
 
 private:
   void _workerLoop();
   std::unique_ptr<Task> _getTask();
-  void _wakeOneWorker();
-  void _wakeAllWorkers();
+  void _doNextTask(std::unique_lock<std::mutex>& taskLock, bool& wakeWorkers);
 
   std::vector<std::unique_ptr<Task>> mTasks;
   std::mutex mTaskMutex;
 
   std::condition_variable mWorkerCV;
-  std::mutex mWorkerCVMutex;
   bool mTerminate;
 
   std::thread* mWorkers;
