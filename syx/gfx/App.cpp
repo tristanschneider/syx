29eb270a334696c17ef5cfdbd7f8722bd725d022
Tristan Schneider <maneugeant@gmail.com>
2017-12-10 23:19:23 -0800

    Imgui test.

--- syx/gfx/App.cpp
+++ syx/gfx/App.cpp
@@ -31,6 +31,8 @@ void App::init() {
   mDefaultSpace->init();
 }
 
+#include "imgui/imgui.h"
+
 void App::update(float dt) {
 
   auto frameTask = std::make_shared<Task>();
@@ -42,6 +44,20 @@ void App::update(float dt) {
   }
   mWorkerPool->queueTask(frameTask);
 
+  if(ImGuiImpl::enabled()) {
+    static float f = 0.0f;
+    ImGui::Text("Hello, world!");
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    static Syx::Vec3 clear_color(0.0f);
+    static bool show_test_window = true;
+    static bool show_another_window = true;
+    ImGui::ColorEdit3("clear color", (float*)&clear_color);
+    if(ImGui::Button("Test Window")) show_test_window ^= 1;
+    if(ImGui::Button("Another Window")) show_another_window ^= 1;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+    const int buffSize = 100;
+    static char buff[buffSize] = { 0 };
+    ImGui::InputText("Text In", buff, buffSize);
   }
 
   std::weak_ptr<Task> weakFrame = frameTask;
