e576ae1470a7e8e4d99e788b20a7abd08bc518f0
Tristan Schneider <maneugeant@gmail.com>
2018-03-23 23:52:42 -0700

    Make static lib for shared and partially fix shared directory intellisense

--- syx/shared/Precompile.h
+++ syx/shared/Precompile.h
@@ -1,1 +1,25 @@
-//Dummy file to fix intellisense thinking it can't open Precompile.h from shared project
+#pragma once
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <stdio.h>
+#include <string>
+#include <algorithm>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <memory>
+#include <mutex>
+#include <type_traits>
+#include <condition_variable>
+#include <thread>
+#include <functional>
+#include <atomic>
+#include <cstdio>
+#include <cassert>
+#include <cstdint>
+
+#include "util/TypeId.h"
