6c570c01422aa0213063c78a9068288f58f1400d
Tristan Schneider <maneugeant@gmail.com>
2020-08-16 23:24:53 -0700

    Async handle with tests

---
+++ syx/test/AsyncHandleTest.cpp
@@ -0,0 +1,124 @@
+#include "Precompile.h"
+#include "CppUnitTest.h"
+
+#include "threading/AsyncHandle.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ThreadingTests {
+  TEST_CLASS(AsyncHandleTests) {
+  public:
+    TEST_METHOD(AsyncResult_CreateValue_HasValue) {
+      const std::string value("value");
+
+      auto asyncResult = Async::createResult(value);
+
+      Assert::AreEqual(*asyncResult, value, L"Value should match", LINE_INFO());
+    }
+
+    //Just make sure the syntax works
+    TEST_METHOD(AsyncResult_CreateVoid_IsEmpty) {
+      auto asyncResult = Async::createResult();
+      *asyncResult;
+    }
+
+    TEST_METHOD(AsyncHandle_CreateVoid_IsPending) {
+      Assert::IsTrue(Async::createAsyncHandle<void>()->getStatus() == AsyncStatus::Pending, L"New task should be pending", LINE_INFO());
+    }
+
+    TEST_METHOD(AsyncHandle_CompleteVoid_IsComplete) {
+      auto handle = Async::createAsyncHandle<void>();
+
+      Async::setComplete(*handle);
+
+      Assert::IsTrue(handle->getStatus() == AsyncStatus::Complete, L"Completed task should have completed state", LINE_INFO());
+    }
+
+    TEST_METHOD(AsyncHandle_CompleteInt_IsComplete) {
+      auto handle = Async::createAsyncHandle<int>();
+
+      Async::setComplete(*handle, 10);
+
+      Assert::IsTrue(handle->getStatus() == AsyncStatus::Complete, L"Completed tasks should have completed state", LINE_INFO());
+      Assert::AreEqual(*handle->getResult(), 10, L"Value should match what was completed", LINE_INFO());
+    }
+
+    TEST_METHOD(AsyncHandle_TakeResult_MatchesGet) {
+      auto handle = Async::createAsyncHandle<std::string>();
+
+      Async::setComplete(*handle, std::string("done"));
+
+      const std::string get = *handle->getResult();
+      const std::string take = *handle->takeResult();
+      Assert::AreEqual(get, take, L"Get value should match take value", LINE_INFO());
+    }
+
+    TEST_METHOD(AsyncHandle_AddCallback_IsCalledOnCompletion) {
+      auto handle = Async::createAsyncHandle<std::string>();
+
+      bool wasCalled = false;
+      const std::string value = "value";
+      handle->then([&wasCalled, &value](IAsyncHandle<std::string>& handle) {
+        Assert::AreEqual(*handle.getResult(), value, L"Should have completed value", LINE_INFO());
+        wasCalled = true;
+      });
+
+      Assert::IsFalse(wasCalled, L"Callback shouldn't be called before task is completed", LINE_INFO());
+
+      Async::setComplete(*handle, value);
+
+      Assert::IsTrue(wasCalled, L"Callback should be called once task is completed", LINE_INFO());
+    }
+
+    TEST_METHOD(AsyncHandle_MultithreadedCallbacks_AllComplete) {
+      auto handle = Async::createAsyncHandle<bool>();
+      const int numCallbacks = 10000;
+      int numComplete = 0;
+      std::atomic_bool threadStarted = false;
+
+      std::thread thread([&handle, &numComplete, &threadStarted] {
+        for(int i = 0; i < 10000; ++i) {
+          handle->then([&numComplete](IAsyncHandle<bool>& result) {
+            Assert::IsTrue(*result.getResult(), L"Value should match completion value", LINE_INFO());
+            ++numComplete;
+          });
+          threadStarted = true;
+        }
+      });
+
+      while(!threadStarted) {
+        std::this_thread::yield();
+      }
+
+      Async::setComplete(*handle, true);
+      thread.join();
+
+      Assert::AreEqual(numCallbacks, numComplete, L"All callbacks should have been triggered", LINE_INFO());
+    }
+
+    TEST_METHOD(CompleteHandle_CreateVoid_IsComplete) {
+      Assert::IsTrue(Async::createCompleteHandle()->getStatus() == AsyncStatus::Complete, L"Complete tasks should have completed state", LINE_INFO());
+    }
+
+    TEST_METHOD(CompleteHandle_CreateInt_IsComplete) {
+      const int value = 10;
+
+      auto handle = Async::createCompleteHandle(value);
+
+      Assert::IsTrue(handle->getStatus() == AsyncStatus::Complete, L"Completed tasks should be complete", LINE_INFO());
+      Assert::AreEqual(*handle->getResult(), value, L"Completed value should match", LINE_INFO());
+    }
+
+    TEST_METHOD(CompleteHandle_AddCallback_IsCalled) {
+      auto handle = Async::createCompleteHandle(10);
+
+      bool wasCalled = false;
+      handle->then([&wasCalled](IAsyncHandle<int>& handle) {
+        Assert::IsTrue(*handle.getResult() == 10, L"Handle should have completed value", LINE_INFO());
+        wasCalled = true;
+      });
+
+      Assert::IsTrue(wasCalled, L"Callback should be called when it is added for compelted results", LINE_INFO());
+    }
+  };
+}
--- todo.md
+++ todo.md
@@ -1,3 +1,4 @@
 - Task should take an async handle (std future?), the task itself shouldn't be the handle as it is now
 - Find a way to make game object modification less confusing when not done through lua. The message dependencies and order aren't obvious.
 - Component access needs to go through LuaGameContext so local changes apply immediately
+- Split LuaNode between reflection and actual lua functionality. It provides an interface to interact with types so it's possible to generically copy and mutate them. On top of this are lua operations, but they're mixed in with the interface. It should be split apart so that extensions like lua can use it without it having to go directly on the interface. For instance, what if a different scripting implementation wanted to use it?
